afafbd36c6c0160f52cf1b31ad9d620a27452b48
rudransh-pokhriyal <134213588+rudransh-pokhriyal@users.noreply.github.com>
2024-01-28 17:27:12 +0000

    log functions

---
+++ operations/lograthmic.java
@@ -0,0 +1,22 @@
+class lograthmic
+{
+    double Log(double a)
+    {
+        return Math.log(a)
+    }
+
+    double expo(double a)
+    {
+        return Math.exp(a)
+    }
+
+    double antilog_e(double a)
+    {
+        return Math.exp(a);
+    }
+
+    double antilog_base(double base,double a)
+    {
+        return Math.pow(base,a);
+    }
+}
