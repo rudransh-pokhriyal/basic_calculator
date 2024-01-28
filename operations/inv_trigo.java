94ece7200a0e0ab2e7b2f694063950a35a6f1557
rudransh-pokhriyal <134213588+rudransh-pokhriyal@users.noreply.github.com>
2024-01-28 17:18:58 +0000

    inverse trignometric functions

---
+++ operations/inv_trigo.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+class inv_trigo.java
+{
+    double Asin(double deg)
+    {
+        return Math.toDegrees(Math.asin(deg));
+    }
+
+    
+    double Acos(double deg)
+    {
+        return Math.toDegrees(Math.acos(deg));
+    }
+
+
+    double Atan(double deg)
+    {
+        return Math.toDegrees(Math.atan(deg));
+    }
+}
